ff0ef3b4427acdaf145d043077cfe10dbe03a073
ummulahid <161658876+ummulahid@users.noreply.github.com>
2024-03-17 22:12:07 +0000

    0000000000000000000d3208

--- static/js/component-siir.js
+++ static/js/component-siir.js
@@ -12,24 +12,32 @@ function siir({name, attrs, styles, props, data, el}){
         ["transition", "all 1s ease"]
     ]}
     => ${({values:v}) => () => {
-        const kita = ch.gatr("kita") || 4;
+        const 
+            kita = ch.gatr("kita") || 4,
+            ayrac = ch.gatr("ayrac") || "***",
+            baslik = ch.gatr("baslik") || "";
         let text = ch.get("textContent");
         v.text = ["", ...text.split(/\n/g).filter(Boolean), ""].map(d => d.trim())
         .reduce((ac,d,i) => {
             ac.push(d);
-            !(i % kita) && ac.push("");
+            !(i % kita) && ac.push(ayrac);
             return ac;
         }, []).join("\n");
+        v.baslik = baslik;
     }}
     >> textContent ${""}
-    +-> ${ch.div}
+    +-> div:${ch.div}
     style ${[
         ["width", "auto"],
         ["padding", "1rem"],
         ["border-image", "url(static/img/ahid-siir/border.png) 20% 20% / 2rem 2rem / 2rem 2rem round"]
     ]}
+    +-> ${ch.p} +-> ${ch.strong}
     => ${({values:v}) => () => {
-        ch.set("textContent", v.text);
+        ch.set("textContent", v.baslik);
+    }}
+    => ${({values:v}) => () => {
+        ch(v.div).set("textContent", v.text);
     }}
     `
 }
