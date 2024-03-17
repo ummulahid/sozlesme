20c09c95e40dfd150c4ddb85c7a2c3e1448f5297
ummulahid <161658876+ummulahid@users.noreply.github.com>
2024-03-17 11:25:43 +0000

    00000000000000075fbd3208

--- static/js/app.js
+++ static/js/app.js
@@ -28,12 +28,6 @@
             "./component-game-card.js"
         )).render
     ]}
-    adopt ...${[
-        "ahid-siir", 
-        (await import(
-            "./component-siir.js"
-        )).render
-    ]}<ahid-siir ${{}}/>
     *> ${"style"} |> sappend ${0}
     >> textContent ${({values}) => values.renderStyles.render(values)}
     0> cont: ${ch.select("#card-container", document.body).selected}
